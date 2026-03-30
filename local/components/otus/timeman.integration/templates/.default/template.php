f5c091dcd413d104da34de1f25ae54978cf94f53
DESKTOP-F46M6NE\user <dima_opalev@mail.ru>
2026-03-30 18:54:34 +0300

    timeman fixed

--- local/components/otus/timeman.integration/templates/.default/template.php
+++ local/components/otus/timeman.integration/templates/.default/template.php
@@ -7,5 +7,3 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 // Добавляем минимальные стили
 \Bitrix\Main\Page\Asset::getInstance()->addCss($templateFolder . '/script.js');
 ?>
-
-<div id="otus-timeman-container"></div>
